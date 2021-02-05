0a58fa585fefa19715ca108c2572bd1657ef7a0f
Rob11001 <r.bruno49@studenti.unisa.it>
2021-02-05 16:33:10 +0100

    Changed setChannel function bindings

--- setChannel/index.js
+++ setChannel/index.js
@@ -1,17 +1,28 @@
+/*
+  Set a new channel for the server(guild)
+*/
 module.exports = async function(context, req) {
   if (req.body && req.body.guild && req.body.channel) {
     // set guild document object
-    context.bindings.guild = {
-      guild: req.body.guild,
-      channel: req.body.channel,
-    };
+    if (context.bindings.guildIn[0]) {
+      // update guild document object
+      context.bindings.guildOut = context.bindings.guildIn[0];
+      context.bindings.guildOut.channel = req.body.channel;
+    } else {
+      // create a new guild document object
+      context.bindings.guildOut = {
+        guild: req.body.guild,
+        channel: req.body.channel,
+        prefix: '!',
+      };
+    }
 
-    context.bindings.res = {
+    return {
       status: '200',
       message: 'the channel has been correctly set',
     };
   } else {
-    context.bindings.res = {
+    return {
       status: '400',
       message: 'Request has some parameters missing',
     };
