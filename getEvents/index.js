ee37a7017460d516e41f8f4ee1c610080e1050d7
emdant <30555419+emdant@users.noreply.github.com>
2021-02-15 19:00:41 +0100

    Changed redis client and order by date

--- getEvents/index.js
+++ getEvents/index.js
@@ -3,7 +3,7 @@ const CosmosClient = new (require('../cosmos/client'))('Events');
 module.exports = async function(context, req) {
   if (req.query.author && req.query.number) {
     const sqlQuery = {
-      query: `SELECT * FROM Events e WHERE e.author = @author ORDER BY e.name OFFSET @offset LIMIT @number`,
+      query: `SELECT * FROM Events e WHERE e.author = @author ORDER BY e.date OFFSET @offset LIMIT @number`,
       parameters: [
         {
           name: '@author',
--- redis/client.js
+++ redis/client.js
@@ -1,26 +1,21 @@
-const redis = require('redis');
-const bluebird = require('bluebird');
+const {Tedis} = require('tedis');
 
-const cacheConnection = redis.createClient({
+const cacheConnection = new Tedis({
   host: process.env.REDIS_HOSTNAME,
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_KEY,
-//  tls: {servername: process.env.REDIS_HOSTNAME}, // Understand
+  //  settare tls
 });
-
-cacheConnection.hgetall = bluebird.promisify(cacheConnection.hgetall).bind(cacheConnection);
-cacheConnection.del = bluebird.promisify(cacheConnection.del).bind(cacheConnection);
-cacheConnection.select = bluebird.promisify(cacheConnection.select).bind(cacheConnection);
 
 class RedisClient {
   static async get(key) {
-    await cacheConnection.select(0);
+    await cacheConnection.command('select', 0);
     return cacheConnection.hgetall(key);
   }
 
   static async del(key) {
-    await cacheConnection.select(0);
-    return cacheConnection.del(key);
+    await cacheConnection.command('select', 0);
+    await cacheConnection.del(key);
   }
 }
 
